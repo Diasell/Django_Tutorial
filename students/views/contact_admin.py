77de75eb04381df93329cd3377cc57ce5a2a84d6
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-03-23 23:09:55 +0200

    added crispy form contact-admin

--- students/views/contact_admin.py
+++ students/views/contact_admin.py
@@ -6,6 +6,10 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 
+#crispy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from studentsdb.settings import ADMIN_EMAIL
 
 
@@ -24,6 +28,27 @@ class ContactForm(forms.Form):
         max_length=2560,
         widget=forms.Textarea
     )
+
+    def __init__(self, *args, **kwargs):
+        # call original initializator
+        super(ContactForm, self).__init__(*args, **kwargs)
+
+        # this helper object allows us to customize form
+        self.helper = FormHelper()
+
+        # form tag attributes
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('contact_admin')
+
+        # twitter bootstrap styles
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # form buttons
+        self.helper.add_input(Submit('send_button', u'Надіслати'))
 
 def contact_admin(request):
     # check if form was posted
@@ -51,9 +76,6 @@ def contact_admin(request):
 
             # redirect to same contact page with  message
             return HttpResponseRedirect(reverse('contact_admin'))
-        else:
-            message = u'Будь ласка заповніть поля форми'
-            messages.warning(request,message)
 
         # if there was not POST render blank form
     else:
