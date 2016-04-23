55d9bc448820b947e12d72d867fb99f4a505913f
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-04-23 13:18:23 +0300

    Added custom logging for Professors

--- students/signals.py
+++ students/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
-from .models import Student
+from .models import Student, Professor
 
 @receiver(post_save, sender=Student)
 def log_student_updated_added_event(sender, **kwargs):
@@ -32,4 +32,31 @@ def log_student_deleted_event(sender, **kwargs):
 
     student = kwargs['instance']
     logger.info("Student deleted: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
+
+
+@receiver(post_save, sender=Professor)
+def log_professor_updated_added_event(sender, **kwargs):
+    """
+    Writes to log info about adding/updating professors
+    """
+    logger = logging.getLogger(__name__)
+
+    proffesor = kwargs['instance']
+    if kwargs['created']:
+        logger.info("Professor added: %s %s (ID: %d)", proffesor.first_name,  proffesor.last_name, proffesor.id)
+    else:
+        logger.info("Professor updated: %s %s (ID: %d)", proffesor.first_name, proffesor.last_name, proffesor.id)
+
+
+
+@receiver(post_delete, sender=Professor)
+def log_professor_deleted_event(sender, **kwargs):
+    """
+    Writes to log about deleting professor events
+    """
+
+    logger = logging.getLogger(__name__)
+
+    professor = kwargs['instance']
+    logger.info("Professor deleted: %s %s (ID: %d)", professor.first_name, professor.last_name, professor.id)
 
