4981e2b12b166a2cc390c73e633ad532dba951f5
Diasell <gutsalyuk.taras@gmail.com>
2016-06-13 17:25:11 +0300

    improved view to track current  weektype

--- students/views/schedule.py
+++ students/views/schedule.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.base import TemplateView
-from ..util import paginate, get_current_group
+from ..util import get_current_group, ifweekiseven
 
-from ..models.students import Student
 from ..models.groups import Group
-from ..models.schedule import WorkingDay, ParaTime, Para
+from ..models.schedule import WorkingDay, Para, StartSemester
 
-from datetime import datetime
-
-from django.contrib import messages
-from django.views.generic import UpdateView, DeleteView
-
-from django.forms import ModelForm, ValidationError
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from crispy_forms.bootstrap import FormActions
-
-
-
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
-from django.utils.translation import ugettext as _
-
+import datetime
 
 
 class ScheduleView(TemplateView):
@@ -34,22 +17,31 @@ class ScheduleView(TemplateView):
 
         # check if we need to show only 1 group of students:
         current_group = get_current_group(self.request)
-        current_weekday = datetime.today().weekday() # integer 0-monday .. 6-Sunday
+        current_weekday = datetime.date.today().weekday()  # integer 0-monday .. 6-Sunday
         weekdays = ['Понеділок', "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота", "Неділя"]
         day = WorkingDay.objects.filter(dayoftheweek=weekdays[current_weekday])
+        # checking what weektype it is now:
+        todaysdate = datetime.date.today()
+        semesters = StartSemester.objects.all()
+        approxsemesterlength = 5*31
+        for semester in semesters:
+            difference = (todaysdate - semester.semesterstart).days
+            if semester.semesterstart <= todaysdate and difference<approxsemesterlength:
+                startsemesterdate = semester.semesterstart
+                weektype = ifweekiseven(todaysdate, startsemesterdate)
+            else:
+                # if weektype is not recognized - uses True as default
+                weektype = True
 
         if current_group:
-            sched_today = Para.objects.filter(para_group=current_group, para_day=day)
-            #ordering
+            sched_today = Para.objects.filter(para_group=current_group, para_day=day, week_type=weektype)
+            # ordering
             sched_today = sched_today.order_by('para_number__para_position')
         else:
             # TODO: what needs to be shown here if group is not defined?
-            sched_today=Para.objects.all()[0]
-
+            sched_today = Para.objects.all()
 
         context['weekday'] = day
         context['sched_today'] = sched_today
         context['groups'] = Group.objects.all().order_by('title')
-
-
         return context
