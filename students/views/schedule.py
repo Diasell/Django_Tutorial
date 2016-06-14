245acb997fb79062f273362a2fa08958aca226f3
Diasell <gutsalyuk.taras@gmail.com>
2016-06-14 14:43:48 +0300

    added schedule for all week to the view

--- students/views/schedule.py
+++ students/views/schedule.py
@@ -19,14 +19,21 @@ class ScheduleView(TemplateView):
         current_group = get_current_group(self.request)
         current_weekday = datetime.date.today().weekday()  # integer 0-monday .. 6-Sunday
         weekdays = ['Понеділок', "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота", "Неділя"]
-        day = WorkingDay.objects.filter(dayoftheweek=weekdays[current_weekday])
+        current_day = WorkingDay.objects.filter(dayoftheweek=weekdays[current_weekday])
+        monday = WorkingDay.objects.filter(dayoftheweek=weekdays[0])
+        tuesday = WorkingDay.objects.filter(dayoftheweek=weekdays[1])
+        wednesday = WorkingDay.objects.filter(dayoftheweek=weekdays[2])
+        thursday = WorkingDay.objects.filter(dayoftheweek=weekdays[3])
+        friday = WorkingDay.objects.filter(dayoftheweek=weekdays[4])
+        saturday = WorkingDay.objects.filter(dayoftheweek=weekdays[5])
+        sunday = WorkingDay.objects.filter(dayoftheweek=weekdays[6])
         # checking what weektype it is now:
         todaysdate = datetime.date.today()
         semesters = StartSemester.objects.all()
         approxsemesterlength = 5*31
         for semester in semesters:
             difference = (todaysdate - semester.semesterstart).days
-            if semester.semesterstart <= todaysdate and difference<approxsemesterlength:
+            if semester.semesterstart <= todaysdate and difference < approxsemesterlength:
                 startsemesterdate = semester.semesterstart
                 weektype = ifweekiseven(todaysdate, startsemesterdate)
             else:
@@ -34,14 +41,32 @@ class ScheduleView(TemplateView):
                 weektype = True
 
         if current_group:
-            sched_today = Para.objects.filter(para_group=current_group, para_day=day, week_type=weektype)
+            sched_today = Para.objects.filter(para_group=current_group, para_day=current_day, week_type=weektype)
+            sched_monday = Para.objects.filter(para_group=current_group, para_day=monday, week_type=weektype)
+            sched_tuesday = Para.objects.filter(para_group=current_group, para_day=tuesday, week_type=weektype)
+            sched_wednesday = Para.objects.filter(para_group=current_group, para_day=wednesday, week_type=weektype)
+            sched_thursday = Para.objects.filter(para_group=current_group, para_day=thursday, week_type=weektype)
+            sched_friday = Para.objects.filter(para_group=current_group, para_day=friday, week_type=weektype)
+            sched_saturday = Para.objects.filter(para_group=current_group, para_day=saturday, week_type=weektype)
             # ordering
             sched_today = sched_today.order_by('para_number__para_position')
+            sched_monday = sched_today.order_by('para_number__para_position')
+            sched_tuesday = sched_today.order_by('para_number__para_position')
+            sched_wednesday = sched_today.order_by('para_number__para_position')
+            sched_thursday = sched_today.order_by('para_number__para_position')
+            sched_friday = sched_today.order_by('para_number__para_position')
+            sched_saturday = sched_today.order_by('para_number__para_position')
         else:
             # TODO: what needs to be shown here if group is not defined?
             sched_today = Para.objects.all()
 
-        context['weekday'] = day
+        context['weekday'] = current_day
         context['sched_today'] = sched_today
+        context['sched_monday'] = sched_monday
+        context['sched_tuesday'] = sched_tuesday
+        context['sched_wednesday'] = sched_wednesday
+        context['sched_thursday'] = sched_thursday
+        context['sched_friday'] = sched_friday
+        context['sched_saturday'] = sched_saturday
         context['groups'] = Group.objects.all().order_by('title')
         return context
